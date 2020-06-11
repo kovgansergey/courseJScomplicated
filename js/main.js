89bce547c2a119cefcb7202b84156649c77c8758
Ковган Сергей Владимирович <Kovgan_SV@Gate.net>
2020-06-11 12:34:50 +0700

    Усложненный урок 4

--- js/main.js
+++ js/main.js
@@ -1,40 +1,17 @@
 "use strict";
 
-// Задача 1
+function test(data) {
+  if (typeof data != 'string') {
+    return 'Аргумент - не является строкой';
+  }
 
-let lang = prompt('Введите язык: ru или en');
-let ruDays = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
-let enDays = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
+  data = data.trim();
 
-console.log('Решение a');
-if (lang === 'ru') {
-  console.log(ruDays);
-} else if (lang === 'en') {
-  console.log(enDays);
-} else {
-  console.log('Вы ввели некоректный язык');
+  if (data.length > 30) {
+    data = data.slice(0, 30) + '...';
+  }
+
+  return data;
 }
 
-console.log('Решение b');
-switch (lang) {
-  case 'ru':
-    console.log(ruDays);
-    break;
-  case 'en':
-    console.log(enDays);
-    break;
-  default:
-    console.log('Вы ввели некоректный язык');
-}
-
-console.log('Решение c');
-let arr = [];
-arr[101110] = enDays;
-arr[114117] = ruDays;
-console.log(arr[Number(String(lang.charCodeAt(0)) + String(lang.charCodeAt(1)))]);
-
-// Задача 2
-
-let namePerson = prompt('Введите имя');
-let result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
-console.log(result);
+console.log(test('            1234567890123456789012345678901        '));
