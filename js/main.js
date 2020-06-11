5d190207b1fb80c42cf0233895f3c329c2172e67
kovgansergey <skydiver.88@mail.ru>
2020-06-11 23:56:29 +0700

    усложненное задание 5

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 "use strict";
 
-function test(data) {
-  if (typeof data != 'string') {
-    return 'Аргумент - не является строкой';
+// Задача 1
+
+let arr = ['51865118', '2489651', '9575124', '75496521', '415683210', '21684416', '668767016'];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
+    console.log(arr[i]);
+  } else {
+    continue;
   }
-
-  data = data.trim();
-
-  if (data.length > 30) {
-    data = data.slice(0, 30) + '...';
-  }
-
-  return data;
 }
 
-console.log(test('            1234567890123456789012345678901        '));
+// Задача 2
+
+let num;
+
+for (let i = 1; i <= 100; i++) {
+  num = 0;
+
+  for (let j = 1; j <= i; j++) {
+    if (Number.isInteger(i / j)) {
+      num++;
+    }
+  }
+  
+  if (num <= 2) {
+    console.log(i + ' Делители этого числа: 1 и ' + i);
+  } else {
+    continue;
+  }
+}
