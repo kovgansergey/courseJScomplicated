09ba0e3ddfbee33dda85133ed9254c9c19ef3cdc
Ковган Сергей Владимирович <Kovgan_SV@Gate.net>
2020-06-16 11:38:50 +0700

    fix: расположил дни начиная с понедельника

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 "use strict";
 
-let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 let date = new Date();
+let today;
+
+if (date.getDay() === 0) {
+  today = 6;
+} else {
+  today = date.getDay() - 1;
+}
 
 for (let i = 0; i < week.length; i++) {
   let day = document.createElement('p');
   day.innerHTML = week[i];
   document.body.appendChild(day);
 
-  if (i === 0 || i === 6) {
+  if (i === 5 || i === 6) {
     day.style.fontStyle = 'italic';
   }
 
-  if (i === date.getDay()) {
+  if (i === today) {
     day.style.fontWeight = 'bold';
   }
 }
