02e9480a54df3296ee6c11ca9e68aa5d96212989
kovgansergey <skydiver.88@mail.ru>
2020-06-16 23:13:30 +0700

    в процессе 9

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let now = new Date();
+
+
+function getTaskA(date) {
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
+
+  return 'Сегодня ' + date.toLocaleString('ru', options) + ' ' + date.getFullYear() + 'года, ' + 
+}
+
+
+let taskA = document.createElement('p');
+taskA.innerHTML = ;
+document.body.appendChild(taskA);
