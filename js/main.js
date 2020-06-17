f7ac3b7b9a83f3cdc4770268d16986c662328287
Ковган Сергей Владимирович <Kovgan_SV@Gate.net>
2020-06-17 12:24:18 +0700

    додеал 9 усложненное

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
-let now = new Date();
-
+let now;
 
 function getTaskA(date) {
   let options = {
@@ -10,10 +9,79 @@ function getTaskA(date) {
     month: 'long',
   };
 
-  return 'Сегодня ' + date.toLocaleString('ru', options) + ' ' + date.getFullYear() + 'года, ' + 
+  function getHoursName(hour) {
+    if (String(hour).charAt(String(hour).length -1) === '1' && hour != 11) {
+      return ' час ';
+    } else if (String(hour).charAt(String(hour).length -1) === '2' && hour != 12) {
+      return ' часа ';
+    } else if (String(hour).charAt(String(hour).length -1) === '3' && hour != 13) {
+      return ' часа ';
+    } else if (String(hour).charAt(String(hour).length -1) === '4' && hour != 14) {
+      return ' часа ';
+    } else {
+      return ' часов ';
+    }
+  }
+
+  function getMinutesName(minute) {
+    if (String(minute).charAt(String(minute).length - 1) === '1' && minute != 11) {
+      return ' минута ';
+    } else if (String(minute).charAt(String(minute).length - 1) === '2' && minute != 12) {
+      return ' минуты ';
+    } else if (String(minute).charAt(String(minute).length - 1) === '3' && minute != 13) {
+      return ' минуты ';
+    } else if (String(minute).charAt(String(minute).length - 1) === '4' && minute != 14) {
+      return ' минуты ';
+    } else {
+      return ' минут ';
+    }
+  }
+
+  function getSecondsName(seconds) {
+    if (String(seconds).charAt(String(seconds).length - 1) === '1' && seconds != 11) {
+      return ' секунда';
+    } else if (String(seconds).charAt(String(seconds).length - 1) === '2' && seconds != 12) {
+      return ' секунды';
+    } else if (String(seconds).charAt(String(seconds).length - 1) === '3' && seconds != 13) {
+      return ' секунды';
+    } else if (String(seconds).charAt(String(seconds).length - 1) === '4' && seconds != 14) {
+      return ' секунды';
+    } else {
+      return ' секунд';
+    }
+  }
+
+  let hoursName = getHoursName(date.getHours());
+  let minutesName = getMinutesName(date.getMinutes());
+  let secondsName = getSecondsName(date.getSeconds());
+
+  return 'Сегодня ' + date.toLocaleString('ru', options) + ' ' + date.getFullYear() + ' года, '
+          + date.getHours() + hoursName + date.getMinutes() + minutesName + date.getSeconds() + secondsName;
 }
 
+function getTaskB(date) {
+  let optionsDate = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  };
+  let optionsTime = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  }
+
+  return date.toLocaleString('ru', optionsDate) + ' - ' + date.toLocaleString('ru', optionsTime);
+}
 
 let taskA = document.createElement('p');
-taskA.innerHTML = ;
 document.body.appendChild(taskA);
+
+let taskB = document.createElement('p');
+document.body.appendChild(taskB);
+
+setInterval(function() {
+  now = new Date();
+  taskA.innerHTML = getTaskA(now);
+  taskB.innerHTML = getTaskB(now);
+}, 1000)
